a1a91e6b8d86c97cddf7dc7eaa952797ee3be2d1
Nattapol Tassaneeyakul <pokneun@gmail.com>
2024-07-28 12:21:06 +0700

    use Service layer at Handler

--- src/main/java/com/springreactive/testwebflux/functionalstyle/handler/MovieInfoHandler.java
+++ src/main/java/com/springreactive/testwebflux/functionalstyle/handler/MovieInfoHandler.java
@@ -4,7 +4,7 @@ import com.springreactive.testwebflux.client.TestRestClient;
 import com.springreactive.testwebflux.domain.MovieInfo;
 import com.springreactive.testwebflux.model.DummyModelInfo;
 import com.springreactive.testwebflux.model.DummyModelResponse;
-import com.springreactive.testwebflux.repository.MovieInfoRepository;
+import com.springreactive.testwebflux.service.MovieInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.ServerRequest;
@@ -15,53 +15,50 @@ import reactor.core.publisher.Mono;
 @Component
 public class MovieInfoHandler {
 
-    private MovieInfoRepository movieInfoRepository;
+    @Autowired
+    private MovieInfoService movieInfoService;
 
     @Autowired
     TestRestClient testRestClient;
 
-    public MovieInfoHandler(MovieInfoRepository movieInfoRepository) {
-        this.movieInfoRepository = movieInfoRepository;
-    }
-
     public Mono<ServerResponse> insertMovie(ServerRequest request) {
         return request.bodyToMono(MovieInfo.class)
-                .flatMap(movieInfoRepository::save)
+                .flatMap(movieInfoService::insertMovie)
                 .flatMap(movieInfo -> ServerResponse.ok().bodyValue(movieInfo));
     }
 
     public Mono<ServerResponse> findAll(ServerRequest request) {
-        Flux<MovieInfo> movieInfoFlux = movieInfoRepository.findAll();
+        Flux<MovieInfo> movieInfoFlux = movieInfoService.findAll();
         return ServerResponse.ok().body(movieInfoFlux, MovieInfo.class);
     }
 
     public Mono<ServerResponse> findById(ServerRequest request) {
         return request.bodyToMono(MovieInfo.class)
-                .flatMap(movieInfoRequest -> movieInfoRepository.findById(movieInfoRequest.getMovieInfoId())
+                .flatMap(movieInfoRequest -> movieInfoService.findById(movieInfoRequest.getMovieInfoId())
                         .flatMap(movieInfo -> ServerResponse.ok().bodyValue(movieInfo)));
     }
 
     public Mono<ServerResponse> findByYear(ServerRequest request) {
         return request.bodyToMono(MovieInfo.class)
-                .map(movieInfoRequest -> movieInfoRepository.findByYear(movieInfoRequest.getYear()))
+                .map(movieInfoRequest -> movieInfoService.findByYear(movieInfoRequest.getYear()))
                 .flatMap(movieInfoByYear -> ServerResponse.ok().body(movieInfoByYear, MovieInfo.class));
     }
 
     public Mono<ServerResponse> updateMovie(ServerRequest request) {
         return request.bodyToMono(MovieInfo.class)
-                .flatMap(movieInfoRequest -> movieInfoRepository.findById(movieInfoRequest.getMovieInfoId())
+                .flatMap(movieInfoRequest -> movieInfoService.findById(movieInfoRequest.getMovieInfoId())
                         .map(movieInfoById -> {
                             movieInfoById.setName(movieInfoRequest.getName() + (int) (Math.random() * 100));
                             return movieInfoById;
                         })
-                        .flatMap(movieInfoRepository::save)
+                        .flatMap(movieInfoService::update)
                         .flatMap(savedMovieInfo -> ServerResponse.ok().bodyValue(savedMovieInfo)));
 
     }
 
     public Mono<ServerResponse> deleteMovie(ServerRequest request) {
         return request.bodyToMono(MovieInfo.class)
-                .flatMap(movieInfoRequest -> movieInfoRepository.deleteById(movieInfoRequest.getMovieInfoId())
+                .flatMap(movieInfoRequest -> movieInfoService.delete(movieInfoRequest.getMovieInfoId())
                         .then(ServerResponse.noContent().build()));
     }
 
